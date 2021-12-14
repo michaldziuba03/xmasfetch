11ec6a7969a0db583c6430c153809237d5d9f8f9
mich4ld <corollid@gmail.com>
2021-12-14 23:00:50 +0100

    print art and info

--- src/info.rs
+++ src/info.rs
@@ -1,19 +1,73 @@
-pub fn get_os() {
+use std::{process::Command, fs::read_to_string, env::var};
 
+fn execute_command(command_name: &str, args: Vec<&str>) -> String {
+    let mut command = Command::new(command_name);
+    command.args(args);
+
+    let output = command.output().expect("Can't execute command");
+    return String::from_utf8(output.stdout).unwrap().trim().to_string();
 }
 
-pub fn get_kernel() {
-
+pub fn get_host() -> String {
+    return execute_command("uname", ["-n"].to_vec());
 }
 
-pub fn get_shell() {
-    
+pub fn get_user() -> String {
+    return var("USER").expect("Can't fetch $USER env var");
 }
 
-pub fn get_uptime() {
+pub fn get_os() -> String {
+    let file = read_to_string("/etc/os-release").expect("Can't get os release data");
+    // Parsing os-release file:
+    let line: Vec<&str> = file
+        .lines()
+        .find(|x| x.contains("NAME"))
+        .unwrap()
+        .split("=")
+        .collect();
 
+    // Removing quotes from string:
+    let value = &line[1].trim();
+    let os = &value[1..value.len()-1];
+
+    return String::from(os);
 }
 
-pub fn get_memory() {
+pub fn get_kernel() -> String {
+    return execute_command("uname", ["-r"].to_vec());
+}
 
+pub fn get_shell() -> String {
+    let shell = var("SHELL").expect("Can't fetch $SHELL env var");
+
+    if shell.is_empty() {
+        return String::from("unknown");
+    }
+
+    let split: Vec<&str> = shell.split("/").collect();
+    let len = split.len();
+    return String::from(split[len - 1]);
+}
+
+pub fn get_uptime() -> String {
+    let uptime = execute_command("uptime", ["-p"].to_vec());
+    let sliced = &uptime[3..uptime.len()];
+    let formated = sliced
+        .replace(" hours", "h")
+        .replace(" hour", "h")
+        .replace(" minutes", "m")
+        .replace(" minute", "m")
+        .replace(" days", "d")
+        .replace(" day", "d")
+        .replace(",", "");
+
+    return String::from(formated);
+}
+
+pub fn get_memory() -> String {
+    let free_result = execute_command("free", ["-m"].to_vec());
+    let lines: Vec<&str> = free_result.lines().collect();
+    let mem_line: Vec<&str> = lines[1].split_whitespace().collect();
+
+    return format!("{}m / {}m", mem_line[2], mem_line[1]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+mod info;
+
 const RED: &str = "\x1b[31m";
 const GREEN: &str = "\x1b[32m";
 const WHITE: &str = "\x1b[0m";
 const BOLD: &str = "\x1b[1m";
-
 
 fn main() {
     let ascii_art = format!(
@@ -22,15 +23,57 @@ fn main() {
     green = GREEN,
 );
 
-    let mut final_result: Vec<String> = vec![];
     let art_lines = ascii_art.lines();
+    let info_vector = get_info();
 
-    for line in art_lines {
-        let str_value = format_info("os".to_string(), "Linux Mint".to_string());
-        println!("{}", str_value);
+    for (i, art_line) in art_lines.enumerate() {
+        let info_element = info_vector.get(i);
+        
+        if info_element.is_none() {
+            println!("{}", art_line);
+        } else {
+            println!("{}  {}", art_line, info_element.unwrap());
+        }
     }
 }
 
-fn format_info(title: String, value: String) -> String {
+fn get_info() -> Vec<String> {
+    let user = info::get_user();
+    let host = info::get_host();
+    let os = info::get_os();
+    let kernel = info::get_kernel();
+    let shell = info::get_shell();
+    let uptime = info::get_uptime();
+    let memory = info::get_memory();
+
+    return [
+        "".to_string(),
+        format_host(user, host),
+        draw_dash(),
+        format_info(" os", os),
+        format_info(" kernel", kernel),
+        format_info(" shell", shell),
+        format_info(" uptime", uptime),
+        format_info(" memory", memory),
+    ].to_vec();
+}
+
+fn format_host(user: String, host: String) -> String {
+    return format!("{bold}{red}{user}{white}@{bold}{red}{host}", 
+        red = RED, 
+        user = user, 
+        white = WHITE, 
+        host = host,
+        bold = BOLD,
+    );
+}
+
+fn draw_dash() -> String {
+    let dash = "────────────────────────────";
+
+    return format!("{}{}{}", BOLD, WHITE, dash);
+}
+
+fn format_info(title: &str, value: String) -> String {
     return format!("{}{}{}: {}{}", BOLD, GREEN, title, WHITE, value);
 }
